3221109b98d49caa22d4fbaec5f5ddf8205111ad
stasis93 <stasis93@yandex.ru>
2017-12-01 20:20:33 +0500

    Water shader

--- shaders/chunk_vs.glsl
+++ shaders/chunk_vs.glsl
@@ -8,11 +8,11 @@ uniform mat4 proj_view;
 uniform float time;
 
 const float pi = 3.1415926;
-const float waterOffsCoeff = 2 * pi / 16;
+const float waterOffsCoeff = 4 * pi / 16;
 
 void main()
 {
-    bool isWater = aPos.w < 0 ? aPos.w == -7 : aPos.w == 7;
+    bool isWater = aPos.w == -7;
     float offs_y = 0;
     if (isWater) {
         offs_y = -0.4 + 0.1 * (sin(time + aPos.x * waterOffsCoeff) +
